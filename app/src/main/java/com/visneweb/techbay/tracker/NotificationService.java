dca7f7f5270c81904d5858cd0d99236a7b3d08b9
cenksimsek1988 <cenksimsek1988@gmail.com>
2018-02-26 01:26:11 +0300

    rssi visibility of selected device launcher logo

---
+++ app/src/main/java/com/visneweb/techbay/tracker/NotificationService.java
@@ -0,0 +1,18 @@
+package com.visneweb.techbay.tracker;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by aureo on 26.02.2018.
+ */
+
+public class NotificationService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent ıntent) {
+        return null;
+    }
+}
